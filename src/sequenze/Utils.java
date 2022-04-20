9acc3b476987fd59e0e72ddc59ce62582cce6d7b
Marco Macrì <macrimarco001@gmail.com>
2022-04-20 13:16:46 +0200

    privatizzo il costruttore di utils

--- src/sequenze/Utils.java
+++ src/sequenze/Utils.java
@@ -1,6 +1,8 @@
 package sequenze;
 
 public class Utils {
+    private Utils(){}
+
     public static void comincia(Thread[]... lista ) throws InterruptedException {
         for (Thread[] t : lista) {
             for(Thread thread : t) thread.start();
